39e742812ad3e9371bc8e29922fb15d80dac633f
Javier Infante <infantejavier@gmail.com>
2026-05-25 15:16:54 -0400

    Update website 05/25/2026 15:16:54.32

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,7 @@ echo Publishing AquaBridge website...
 echo.
 git add .
 git commit -m "Update website %date% %time%"
+git pull --rebase origin main
 git push
 echo.
 echo Done! Changes will be live at www.aquabridge.cl in about a minute.
