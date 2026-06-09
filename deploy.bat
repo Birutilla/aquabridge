666b4e4f50610fcde25228d27d4f56a95421670a
Javier Infante <infantejavier@gmail.com>
2026-06-09 08:59:48 -0400

    Update website 06/09/2026  8:59:48.52

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,16 @@ echo Publishing AquaBridge website...
 echo.
 git add .
 git commit -m "Update website %date% %time%"
-git pull --rebase origin main
+
+:: Clean stale lock files
+for /r ".git" %%f in (*.lock) do del /f /q "%%f" 2>nul
+
+:: Remove loose origin/main ref if it conflicts with packed-refs
+if exist ".git\refs\remotes\origin\main" del /f /q ".git\refs\remotes\origin\main"
+
+:: Fetch and rebase separately for reliability
+git fetch origin main
+git rebase origin/main
 git push origin HEAD:main
 echo.
 echo Done! Changes will be live at www.aquabridge.cl in about a minute.
